55d4f2b5629ccf7e8508e90792fb04143067b08d
marcinrejdych <marcin.rejdych@relayr.io>
2018-01-21 00:05:49 +0100

    mr: added swagger, changes in build.gradle, dockerfiles, docker-compose and configuration

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM openjdk:8
 
-WORKDIR /opt/jalgoarena-auth
-ADD build/libs/jalgoarena-auth-*.jar /opt/jalgoarena-auth
+
+ADD build/libs/jalgoarena-auth-*.jar /opt/jalgoarena-auth/jalgoarena-auth.jar
 
 ENV EUREKA_URL=http://eureka:5000/eureka
 EXPOSE 9999
 
-CMD java -Dserver.port=9999 -jar /opt/jalgoarena-auth/jalgoarena-auth-*.jar
+ENTRYPOINT java -Dserver.port=9999 -jar /opt/jalgoarena-auth/jalgoarena-auth.jar
--- src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
@@ -4,12 +4,29 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigurationProperties
-open class JAlgoArenaAuthServer
+@Configuration
+@EnableSwagger2
+open class JAlgoArenaAuthServer {
+
+    @Bean
+    open fun jalGoArenaApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.jalgoarena"))
+                .paths(PathSelectors.any()).build()
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(JAlgoArenaAuthServer::class.java, *args)
